1cfa49de33574f3e5aea53518c7fe8197dd174f2
Carmen Rata <carmen.rata@windriver.com>
2020-04-16 19:21:55 -0400

    Verify-license refactoring
    Added variable number of arguments capability and
    cleaned up unused code.
    Allowing variable number of arguments would help different
    vendors implement their own version of licensing that might
    need additional information to be passed to the verify_license
    function, e.g. software version.
    
    Story: 2007403
    Task: 39407
    
    Change-Id: I5c7639fb77e7a2a3edd3c99a8b29b0761cd444c4
    Signed-off-by: Carmen Rata <carmen.rata@windriver.com>

--- utilities/platform-util/platform-util/platform_util/license/license.py
+++ utilities/platform-util/platform-util/platform_util/license/license.py
@@ -1,30 +1,35 @@
 #
-# Copyright (c) 2017-2019 Wind River Systems, Inc.
+# Copyright (c) 2017-2020 Wind River Systems, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-import logging
 import os
 from platform_util.license import exception
 import sys
 
-LOG = logging.getLogger(__name__)
 
-def verify_license(license_file):
-    """Verify all features in a license file"""
-    if not os.path.isfile(license_file):
+def verify_license(*args):
+    """Verify the license file"""
+    if not os.path.isfile(args[0]):
         raise exception.LicenseNotFound()
 
 
 def main():
-    if len(sys.argv) == 2:
-        licensefile = sys.argv[1]
-    else:
-        print("Usage: verify-license <license file>")
+    # Pass the command arguments to verify_license
+    if len(sys.argv) < 2:
+        print("Usage: verify-license <license file> [<optional_parameter>...]")
         exit(-1)
 
+    arg_list = []
+    for arg in sys.argv:
+        arg_list.append(arg)
+
+    # The arguments passed to verify_license from command line
+    # will not include sys.argv[0] which is the script name.
+    # Only the actual arguments: sys.argv[1] and onward will be passed,
+    # meaning license_file followed by optional attributes.
     try:
-        verify_license(licensefile)
+        verify_license(*arg_list[1:len(sys.argv)])
     except exception.InvalidLicenseType:
         exit(1)
     except exception.LicenseNotFound:
