5c8c8434389d67a24758ea36b9439ef946c46dfd
Jose Infanzon <jose.infanzon@windriver.com>
2020-12-16 17:25:18 -0300

    Add new logs to the inventory info files
    Now collect_tool implements a new param
    '-i' when inventory info is required to
    be retrieved. By default it will not be
    executed.
    
    The following command outputs were added:
    system show
    system host-show <hostname>
    system host-port-list <hostname>
    system host-if-list <hostname>
    system interface-network-list <hostname>
    system network-list
    system host-memory-list <hostname>
    system host-label-list <hostname>
    system host-disk-list <hostname>
    system host-stor-list <hostname>
    system host-lvg-list <hostname>
    system host-pv-list <hostname>
    
    the execution of:
    
    -system host-show
    -system host-port-list
    -system host-if-list
    -system interface-network-list
    -system host-ethernet-port-list
    -system host-memory-list
    -system host-label-list
    -system host-disk-list
    -system host-stor-list
    -system host-lvg-list
    -system host-pv-list
    
    on a simplex lab, took 12 seconds to complete
    
    Story: 2008452
    Task: 41428
    
    Signed-off-by: Jose Infanzon <jose.infanzon@windriver.com>
    Change-Id: I223a3ef239a00a1e9dddb86d04874f13c33163e9

--- tools/collector/scripts/collect_sysinv.sh
+++ tools/collector/scripts/collect_sysinv.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-# Copyright (c) 2013-2014 Wind River Systems, Inc.
+# Copyright (c) 2013-2020 Wind River Systems, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,6 +13,7 @@ source /usr/local/sbin/collect_utils
 SERVICE="inventory"
 LOGFILE="${extradir}/${SERVICE}.info"
 RPMLOG="${extradir}/rpm.info"
+INVENTORY=${4}
 
 function is_service_active {
     active=`sm-query service management-ip | grep "enabled-active"`
@@ -21,6 +22,74 @@ function is_service_active {
     else
         return 1
     fi
+}
+
+function collect_inventory {
+    is_service_active
+    if [ "$?" = "0" ] ; then
+        exit 0
+    fi
+    echo    "${hostname}: System Inventory ..: ${LOGFILE}"
+
+    HOSTNAMES=$(system host-list --nowrap | grep '[0-9]' | cut -d '|' -f 3 | tr -d ' ')
+    if [[ -z ${HOSTNAMES} || ${HOSTNAMES} != *"controller"* ]];  then
+        echo "Failed to get system host-list" > $LOGFILE
+        exit 0
+    fi
+
+    # These go into the SERVICE.info file
+    delimiter ${LOGFILE} "system show"
+    system show 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+    delimiter ${LOGFILE} "system host-list"
+    system host-list 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+    delimiter ${LOGFILE} "system datanetwork-list"
+    system datanetwork-list 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+    delimiter ${LOGFILE} "system service-list"
+    system service-list 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+   # delimiter ${LOGFILE} "vm-topology"
+   # timeout 60 vm-topology --show all 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+    delimiter ${LOGFILE} "system network-list"
+    system network-list 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+    for host in ${HOSTNAMES}; do
+        delimiter ${LOGFILE} "system host-show ${host}"
+        system host-show 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+        delimiter ${LOGFILE} "system host-port-list ${host}"
+        system host-port-list ${host} 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+        delimiter ${LOGFILE} "system host-if-list ${host}"
+        system host-if-list ${host} 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+        delimiter ${LOGFILE} "system interface-network-list ${host}"
+        system interface-network-list ${host} 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+        delimiter ${LOGFILE} "system host-ethernet-port-list ${host}"
+        system host-ethernet-port-list ${host} 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+        delimiter ${LOGFILE} "system host-memory-list ${host}"
+        system host-memory-list ${host} 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+        delimiter ${LOGFILE} "system host-label-list ${host}"
+        system host-label-list ${host} 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+        delimiter ${LOGFILE} "system host-disk-list ${host}"
+        system host-disk-list ${host} 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+        delimiter ${LOGFILE} "system host-stor-list ${host}"
+        system host-stor-list ${host} 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+        delimiter ${LOGFILE} "system host-lvg-list ${host}"
+        system host-lvg-list ${host} 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+
+        delimiter ${LOGFILE} "system host-pv-list ${host}"
+        system host-pv-list ${host} 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
+    done
 }
 
 ###############################################################################
@@ -33,37 +102,9 @@ if [ "$nodetype" = "controller" ] ; then
     delimiter ${RPMLOG} "rpm -qa"
     rpm -qa >> ${RPMLOG}
 
-    is_service_active
-    if [ "$?" = "0" ] ; then
-        exit 0
+    if [ "${INVENTORY}" = true ] ; then
+        collect_inventory
     fi
-
-    echo    "${hostname}: System Inventory ..: ${LOGFILE}"
-
-    # These go into the SERVICE.info file
-    delimiter ${LOGFILE} "system host-list"
-    system host-list 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
-
-    delimiter ${LOGFILE} "system service-list"
-    system service-list 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
-
-    delimiter ${LOGFILE} "nova service-list"
-    nova service-list 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
-
-    delimiter ${LOGFILE} "neutron host-list"
-    neutron host-list 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
-
-    delimiter ${LOGFILE} "system host-port-list controller-0"
-    system host-port-list controller-0 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
-
-    delimiter ${LOGFILE} "system host-port-list controller-1"
-    system host-port-list controller-1 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
-
-    delimiter ${LOGFILE} "Dump all Instances"
-    nova list --fields name,status,OS-EXT-SRV-ATTR:host --all-tenant 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
-
-    delimiter ${LOGFILE} "vm-topology"
-    timeout 60 vm-topology --show all 2>>${COLLECT_ERROR_LOG} >> ${LOGFILE}
 
     # copy /opt/platform to extra dir while filtering out the
     # iso and lost+found dirs
