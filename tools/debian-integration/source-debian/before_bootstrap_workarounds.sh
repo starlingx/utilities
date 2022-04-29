5959d5ca1b0462f055f144db79e2a34d04320289
Andre Fernando Zanella Kantek <AndreFernandoZanella.Kantek@windriver.com>
2022-04-29 08:26:21 -0300

    debian-integration: remove DNS workaround BI 40
    It is no longer necessary to use this workaround since the bootstrap
    is already capable to set the correct values in resolv.conf if the
    server values are present on localhost.yaml.
    
    Test Plan:
    PASS: install AIO-SX with Debian
    PASS: ansible bootstrap without the workaround
    PASS: host unlock
    
    Story: 2009965
    Task: 45217
    
    Signed-off-by: Andre Fernando Zanella Kantek <AndreFernandoZanella.Kantek@windriver.com>
    Change-Id: Ieaa931aed16b947b054e14cb2e852dca0f6201fa

--- tools/debian-integration/source-debian/before_bootstrap_workarounds.sh
+++ tools/debian-integration/source-debian/before_bootstrap_workarounds.sh
@@ -15,21 +15,6 @@ ostree admin unlock --hotfix
 hostnamectl set-hostname localhost
 
 # sudo instant response and download stuff
-# Workaround BI 40: need to generate the correct dns with access to private docker registry
-cat > /home/sysadmin/correct_dns.py <<EOF
-#!/usr/bin/env python
-import yaml
-with open("/etc/resolv.conf", "w") as f:
-    with open("/home/sysadmin/localhost.yml", "r") as stream:
-        try:
-            data = yaml.safe_load(stream)
-            for dns in data['dns_servers']:
-                    f.write('nameserver ' + dns)
-                    f.write('\n')
-        except yaml.YAMLError as exc:
-            print(exc)
-EOF
-sudo python /home/sysadmin/correct_dns.py
 
 # BI 20 e and n:
 bifile='/home/sysadmin/.bi20e'
