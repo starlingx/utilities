4537caca9842447b4b9d2d01689681df0690910c
Al Bailey <al.bailey@windriver.com>
2022-04-29 22:44:54 +0000

    Update workarounds from 22.02 to 22.06
    When the upversion to 22.06 occurred, puppet fails
    due to a workaround referencing the old version.
    
    Replacing one hard coded version with another since
    these workarounds are expected to all be removed
    in the next few weeks.
    
    Test Plan:
      Build / Boot / Bootstrap /Unlock on Debian
    
    Story: 2009754
    Task: 45229
    Signed-off-by: Al Bailey <al.bailey@windriver.com>
    Change-Id: I6f4741ec4cafc58591252e45af52ef5c7fda6c8f

--- tools/debian-integration/source-debian/before_bootstrap_workarounds.sh
+++ tools/debian-integration/source-debian/before_bootstrap_workarounds.sh
@@ -48,9 +48,9 @@ sed -i "s@python-psycopg2@python3-psycopg2@g" /usr/share/puppet/modules/postgres
 # BI 20.o:
 bifile='/home/sysadmin/.20o'
 if [ ! -f ${bifile} ]; then
-  sed -i 's@/var/lib/postgresql/%I@/var/lib/postgresql/22.02@g' /lib/systemd/system/postgresql@.service
-  sed -i 's@/var/lib/postgresql/13/main@/var/lib/postgresql/22.02@g' /etc/postgresql/13/main/postgresql.conf
-  sed -i 's@ExecStart=-/usr/bin/pg_ctlcluster --skip-systemctl-redirect %i start@ExecStart=-/usr/bin/pg_ctlcluster --skip-systemctl-redirect %i start -- -D /var/lib/postgresql/22.02@g' /lib/systemd/system/postgresql@.service
+  sed -i 's@/var/lib/postgresql/%I@/var/lib/postgresql/22.06@g' /lib/systemd/system/postgresql@.service
+  sed -i 's@/var/lib/postgresql/13/main@/var/lib/postgresql/22.06@g' /etc/postgresql/13/main/postgresql.conf
+  sed -i 's@ExecStart=-/usr/bin/pg_ctlcluster --skip-systemctl-redirect %i start@ExecStart=-/usr/bin/pg_ctlcluster --skip-systemctl-redirect %i start -- -D /var/lib/postgresql/22.06@g' /lib/systemd/system/postgresql@.service
   systemctl daemon-reload
 
   sed -i '86 a \ \ Anchor["postgresql::server::service::end"] ->' /usr/share/puppet/modules/postgresql/manifests/server/role.pp
