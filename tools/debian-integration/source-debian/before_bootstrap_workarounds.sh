cb4e7f1531e10fea1d39b19fcd3eca2b4c5a5ba0
Charles Short <charles.short@windriver.com>
2022-05-04 10:20:38 -0400

    debian: Remove postgresql workarounds
    Removed BI20, BI50, BI38 workarounds
    
    Story: 2009101
    Task: 43326
    
    Signed-off-by: Charles Short <charles.short@windriver.com>
    Change-Id: I4770f1f9f49f2a427bd6816a68dae82de62b66f5

--- tools/debian-integration/source-debian/before_bootstrap_workarounds.sh
+++ tools/debian-integration/source-debian/before_bootstrap_workarounds.sh
@@ -28,32 +28,6 @@ with open("/etc/resolv.conf", "w") as f:
 EOF
 sudo python /home/sysadmin/correct_dns.py
 
-# BI 20 e and n:
-bifile='/home/sysadmin/.bi20e'
-if [ ! -f ${bifile} ]; then
-  for f in /usr/lib/postgresql/13/bin/*
-  do
-    echo "Linked $f"
-    ln -s "$f" /usr/bin
-  done
-  touch ${bifile}
-fi
-
-# BI 20 f:
-sed -i "s@python-psycopg2@python3-psycopg2@g" /usr/share/puppet/modules/postgresql/manifests/params.pp
-
-# BI 20.o:
-bifile='/home/sysadmin/.20o'
-if [ ! -f ${bifile} ]; then
-  sed -i 's@/var/lib/postgresql/%I@/var/lib/postgresql/22.06@g' /lib/systemd/system/postgresql@.service
-  sed -i 's@/var/lib/postgresql/13/main@/var/lib/postgresql/22.06@g' /etc/postgresql/13/main/postgresql.conf
-  sed -i 's@ExecStart=-/usr/bin/pg_ctlcluster --skip-systemctl-redirect %i start@ExecStart=-/usr/bin/pg_ctlcluster --skip-systemctl-redirect %i start -- -D /var/lib/postgresql/22.06@g' /lib/systemd/system/postgresql@.service
-  systemctl daemon-reload
-
-  sed -i '86 a \ \ Anchor["postgresql::server::service::end"] ->' /usr/share/puppet/modules/postgresql/manifests/server/role.pp
-  touch ${bifile}
-fi
-
 # BI 25: ignore puppet apply warnings until we fix them
 sed -i 's@Warning|@MMAAAAAAAAAASKED|@g' /usr/local/bin/puppet-manifest-apply.sh
 
@@ -65,17 +39,6 @@ if [ ! -f ${bifile} ]; then
 
   touch ${bifile}
 fi
-
-# BI 40: workaround located at bootstrap section
-
-# BI 50: postgres configuration issue
-sed -i 's@#listen_addresses = '\''localhost'\''@listen_addresses = '\''*'\''@g' /etc/postgresql/13/main/postgresql.conf
-echo "host    all             all             0.0.0.0/0               md5" >> /etc/postgresql/13/main/pg_hba.conf
-# ipv6
-echo "host    all             all             ::0/0                   md5" >> /etc/postgresql/13/main/pg_hba.conf
-
-# BI 38.b: slow rpc calls.
-echo "jit = off" >> /etc/postgresql/13/main/postgresql.conf
 
 # Debian ClusterIssuer cert-manager v1 is not currently available for test-issuer during bootstrap
 bifile='/home/sysadmin/.cm'
