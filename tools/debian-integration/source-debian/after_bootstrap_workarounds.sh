2fbc6cae3b65935488a83b2b907ee61afb379f4d
Fabricio Henrique Ramos <fabriciohenrique.ramos@windriver.com>
2022-05-24 20:41:56 -0300

    debian: remove unlock workaround 20
    Removing this workaround since it has been fixed by the review
    in the Depends-On entry below.
    
    There was already a commit to remove this workaround [1], because
    it was not reproducible anymore, but after its removal another error,
    still related, showed up during unlock, therefore the commit was
    reverted [2], since it broke the unlock process.
    
    This time the issue has actually been fixed, thus this removal
    is safe.
    
    Test Plan:
    Debian/CentOS:
    PASS: global_filter entry on /etc/lvm/lvm.conf updated correctly
    PASS: Controller unlocked/enabled/available
    
    Story: 2009965
    Task: 45439
    
    [1] https://review.opendev.org/c/starlingx/utilities/+/842641
    [2] https://review.opendev.org/c/starlingx/utilities/+/842943
    
    Depends-On: https://review.opendev.org/c/starlingx/stx-puppet/+/843223
    
    Signed-off-by: Fabricio Henrique Ramos <fabriciohenrique.ramos@windriver.com>
    Change-Id: Id0c821987683d1428a1f34ac6fd7bf1e97add85e

--- tools/debian-integration/source-debian/after_bootstrap_workarounds.sh
+++ tools/debian-integration/source-debian/after_bootstrap_workarounds.sh
@@ -1,7 +1,3 @@
-# UAR 20: lvm.conf issue
-sed -i "s@match => '\^\[ ]\*global_filter =',@match => '\^\[ \\\t]\*#? ?global_filter =',@g" /usr/share/puppet/modules/platform/manifests/worker.pp
-sed -i "s@match => '\^\[ ]\*global_filter =',@match => '\^\[ \\\t]\*#? ?global_filter =',@g" /usr/share/puppet/modules/platform/manifests/lvm.pp
-
 # UAR 10: disable
 find /etc/puppet/manifests -type f -exec \
   sed -i 's@include ::platform::ptp@#include ::platform::ptp@g' {} +
