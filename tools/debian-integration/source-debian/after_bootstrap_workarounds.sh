85358419b9e915b41a4949f3267bb53d5446f01b
Frank Miller <frank.miller@windriver.com>
2022-05-31 20:09:18 +0000

    Revert "Remove UAR 28 (grub workaround)"
    This reverts commit b079a33c13099cb769c385b28a7c4608a9001746.
    
    Reason for revert: Since commit 
    0f6eb74ed7415defe94dc8b92c5cb9c16aee8b26 is being reverted,
    this commit also needs to be reverted for now.
    
    Change-Id: Ife3422581512ae44c0f4a408d70ecd19bd75e090

--- tools/debian-integration/source-debian/after_bootstrap_workarounds.sh
+++ tools/debian-integration/source-debian/after_bootstrap_workarounds.sh
@@ -7,3 +7,7 @@ find /etc/puppet/manifests -type f -exec \
   sed -i 's@include ::platform::ptp@#include ::platform::ptp@g' {} +
 #find /etc/puppet/manifests -type f -exec \
 #  sed -i 's@include ::platform::ptpinstance@#include ::platform::ptpinstance@g' {} +
+
+# UAR 28: not a fix
+sed -i "s@command => 'reboot',@command => 'ls'@g" /usr/share/puppet/modules/platform/manifests/compute.pp
+
