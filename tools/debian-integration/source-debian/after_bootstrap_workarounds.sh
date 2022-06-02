6b40f82cf09dad1c479d0fc12f4b35ed21604cb9
Douglas Henrique Koerich <douglashenrique.koerich@windriver.com>
2022-06-02 20:47:53 -0300

    [PTP on Debian] Remove PTP from workarounds
    With the change
    https://review.opendev.org/c/starlingx/stx-puppet/+/843335 the PTP
    configuration done by Puppet should be run till the end without errors,
    since the proper directories inside /etc in Debian are used.
    This change removes the workaround from the "after bootstrap" script
    that has hidden the PTP classes in Puppet manifests.
    
    Task: 45538
    Story: 2009965
    Depends-On: https://review.opendev.org/c/starlingx/stx-puppet/+/843335
    Signed-off-by: Douglas Henrique Koerich <douglashenrique.koerich@windriver.com>
    Change-Id: Ia98ff2a40d1efc1c0367f7f1112ac46132b5681d

--- tools/debian-integration/source-debian/after_bootstrap_workarounds.sh
+++ tools/debian-integration/source-debian/after_bootstrap_workarounds.sh
@@ -1,5 +1,2 @@
 # UAR 10: disable
-find /etc/puppet/manifests -type f -exec \
-  sed -i 's@include ::platform::ptp@#include ::platform::ptp@g' {} +
-#find /etc/puppet/manifests -type f -exec \
-#  sed -i 's@include ::platform::ptpinstance@#include ::platform::ptpinstance@g' {} +
+
