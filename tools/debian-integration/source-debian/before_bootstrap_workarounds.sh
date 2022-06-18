3596ede214d9211164733d5ad0df0dd640d66b9a
Dan Voiculeasa <dan.voiculeasa@windriver.com>
2022-06-18 09:28:48 +0300

    debian: Remove ostree unlock workaround
    Debian integration progressed. We don't need to unlock ostree.
    Disable the ostree unlock workaround.
    
    Tests on AIO-SX:
    PASS: unlocked enabled available
    
    Story: 2009964
    Task: 45649
    Signed-off-by: Dan Voiculeasa <dan.voiculeasa@windriver.com>
    Change-Id: Ia0e2011eb9e66c6410b7a7b9c80cef88d9898e4d

--- tools/debian-integration/source-debian/before_bootstrap_workarounds.sh
+++ tools/debian-integration/source-debian/before_bootstrap_workarounds.sh
@@ -1,16 +1,1 @@
-# syntactic sugar
-echo 'set mouse-=a' > ~/.vimrc
-#############################
-# WORKAROUNDS PRE-BOOTSTRAP #
-#############################
-# May want to sudo source this
-# WARNING: Everything here was run under root user.
-
-# NOTE: some of the issues may be hidden by this remount, ideally it should be reverted after each command. Don't expect many such issues so leave it as it is for now.
-# mount -o remount,rw /usr
-# mount -o remount,rw /
-ostree admin unlock --hotfix
-
-# sudo instant response and download stuff
-
-
+# don't remove the file yet, keep this hot
