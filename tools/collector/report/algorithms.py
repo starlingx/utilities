f7f3c59410f9152fcc0a58f762ad3ad7003a9f15
Yang Lu <yang.lu@windriver.com>
2022-07-21 13:11:53 -0400

    Report Tool: Log Event Reporter
    The Report tool helps with the debugging process by aggregating
    relevant log events and information from collect bundle, and
    presenting them in output files.
    
    The tool allows users to create plugin files which contain
    specifications for what log events/information to gather.
    
    Users also have the option of using information gathering
    algorithms directly in the command line without creating
    a plugin file.
    
    The tool features context sensitive help messages for
    every algorithm.
    
    Tests:
    
    PASS: Verify substring algorithm is working
    PASS: Verify alarm algorithm is working
    PASS: Verify system info algorithm is working
    PASS: Verify swact activity algorithm is working
    PASS: Verify puppet log error algorithm is working
    PASS: Verify process failure algorithm is working
    PASS: Verify the tool works on different collect bundles
    PASS: Verify context sensitive help is working
    PASS: Verify running algorithms in command line is working
    PASS: Verify plugin file verification is working
    PASS: Verify plugins do not take too long to run
    PASS: Verify report tool logging logs proper info and errors
    
    Story: 2010166
    Task: 45841
    Signed-off-by: Yang Lu <yang.lu@windriver.com>
    Change-Id: I6f2439e5268a10bf5c70712c8863a6893c1a16b9

---
+++ tools/collector/report/algorithms.py
@@ -0,0 +1,16 @@
+########################################################################
+#
+# Copyright (c) 2022 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+########################################################################
+
+# Algorithm string constants
+ALARM = "alarm"
+AUDIT = "audit"
+PROCESS_FAILURE = "process_failure"
+PUPPET = "puppet"
+SUBSTRING = "substring"
+SWACT = "swact"
+SYSTEM_INFO = "system_info"
