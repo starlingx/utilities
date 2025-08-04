93dad4fe0639cedeba01c2442aa84694d5f7f7e9
Michel Thebeau <Michel.Thebeau@windriver.com>
2025-08-04 12:47:00 +0000

    debrand the monitor service
    Remove most of the branded references in comment, log and variable
    names.  This reduces the cost of code reuse
    in change I0d7ff44d92bb6454101c6763bed29d9d8c92c136
    
    Partial-bug: 2117422
    
    Test Plan:
    PASS  Unit test changes
    PASS  Host-based openbao
    PASS  K8s application openbao
    
    Change-Id: I576af7808c7dbd00618d08ce7686303be27a3363
    Signed-off-by: Michel Thebeau <Michel.Thebeau@windriver.com>

--- security/openbao-monitor/source/config/validate.go
+++ security/openbao-monitor/source/config/validate.go
@@ -10,12 +10,12 @@ import (
 )
 
 func (configInstance MonitorConfig) validateDNS() error {
-	for domain_name, url := range configInstance.OpenbaoAddresses {
+	for domain_name, url := range configInstance.ServerAddresses {
 		// If Host is empty, then the domain entry is invalid
 		// The ports will always at least have the default value of 8200
 		if url.Host == "" {
 			return fmt.Errorf(
-				"the domain entry %v in OpenbaoAddresses is invalid", domain_name)
+				"the domain entry %v in ServerAddresses is invalid", domain_name)
 		}
 	}
 
